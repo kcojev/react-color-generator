bfa830570d5a836a9138a5d14dbbeb517943d08c
Georgy Kcojev <kcojev@gmail.com>
2023-01-20 19:50:56 +0300

    app basic structure, form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
+const [color, setColor] = useState('')
+const [error, setError] = useState(false)
+const [list, setList] = useState([])
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log('hello')
+}
+
 
 function App() {
-  return <h2>color generator project</h2>
+  return (
+    <>
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          value={color} 
+          onChange={(e) => setColor(e.target.value)}
+          placeholder='#f15025'
+        />
+        <button type='submit' className="btn">
+          submit
+        </button>
+      </form>
+    </section>
+    <section className="classes">
+      <h4>list goes here</h4>
+    </section>
+    </>
+  )
 }
 
 export default App
